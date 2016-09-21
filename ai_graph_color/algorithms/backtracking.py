75668ddd1362898a25530e437888a82555936234
Sage Smith <sage.smith2@bz-cs-cslab2l.msu.montana.edu>
2016-09-21 17:57:29 -0600

    Added backtracking shell

---
+++ ai_graph_color/algorithms/backtracking.py
@@ -0,0 +1,14 @@
+def run(setup, problem, *params):
+    """
+    This will execute the algorithm
+
+    :param setup: the setup for this algorithm
+    :type setup: :class: ~ai_graph_color.Setup
+    :param problem: the graph to color
+    :type problem: the graph to color
+    :param params: the params for this algorithm
+    :type params: tuple
+    :rtype: list[int] the list of coloring
+    :return: the list of colorings for this graph
+    """
+    pass
