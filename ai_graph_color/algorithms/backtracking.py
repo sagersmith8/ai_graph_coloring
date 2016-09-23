5975b934b6a44d4ba04295b936cd1d3059199ccc
Sage Smith <sage.smith2@bz-cs-cslab5l.msu.montana.edu>
2016-09-23 01:29:36 -0600

    Finished backtracking with forward checking

--- ai_graph_color/algorithms/backtracking.py
+++ ai_graph_color/algorithms/backtracking.py
@@ -30,12 +30,13 @@ def backtracking_search(graph, num_colors):
                 iteration, stack
             )
         )
-
+        if len(stack) == 0 or complete(coloring, graph):
+            return coloring
         cur_node = stack[len(stack)-1][1]
         coloring[cur_node] = stack[len(stack)-1][3]
-        choose_next_node(stack, coloring, graph, avail_colors, num_colors)
+        if not choose_next_node(stack, coloring, graph, avail_colors, num_colors):
+            return coloring
         iteration += 1
-    return coloring
 
 
 def init(graph, num_colors):
@@ -44,15 +45,7 @@ def init(graph, num_colors):
         avail_colors.append(set(xrange(num_colors)))
     coloring = dict()
     stack = list()
-    cur_node = min_remaining_var(coloring, graph)
-    chosen_color = min_color_conflicts(
-        avail_colors, graph, cur_node, num_colors
-    )
-
-    for node in graph[cur_node]:
-        avail_colors[node] -= {coloring[cur_node]}
-
-    stack.append([coloring, cur_node, avail_colors, chosen_color])
+    choose_next_node(stack, coloring, graph, avail_colors, num_colors)
     return (
         coloring,
         avail_colors,
@@ -74,21 +67,32 @@ def choose_next_node(stack, coloring, graph, avail_colors, num_colors):
         min_remaining_var(coloring, graph)
     )
 
-    for node in graph[next_node]:
-        avail_colors[node] -= {coloring[next_node]}
+    while next_node is None:
+        if len(stack) == 0:
+            return False
+        stack.pop()
+        next_node = (
+            min_remaining_var(coloring, graph)
+        )
 
-    min_color_conflicts(
+    chosen_color = min_color_conflicts(
         avail_colors[next_node], graph, next_node, num_colors
     )
 
+    for node in graph[next_node]:
+        avail_colors[node] -= {chosen_color}
+
     if len(avail_colors[next_node]) > 0:
         stack.append(
-            coloring,
-            next_node,
-            avail_colors[next_node],
+            [
+                coloring,
+                next_node,
+                avail_colors[next_node],
+                chosen_color
+            ]
         )
-    else:
-        stack.pop()
+        return True
+    return False
 
 
 def min_color_conflicts(avail_colors, graph, cur_node, num_color):
@@ -107,8 +111,7 @@ def min_color_conflicts(avail_colors, graph, cur_node, num_color):
     all_colors = set(range(num_color))
     for node in graph[cur_node]:
         available_colors = (
-            avail_colors[node]-(all_colors -
-                                avail_colors[cur_node])
+            avail_colors-(all_colors - avail_colors)
         )
         for color in available_colors:
             available_color_count[color][0] += 1
@@ -144,16 +147,18 @@ def min_remaining_var(coloring, graph):
 
     for index, adj_list in enumerate(graph):
         colored_neighbors = set()
+        if coloring.get(index) is not None:
+            continue
         for node in adj_list:
             if coloring.get(node) is not None:
                 colored_neighbors.add(coloring.get(node))
         num_neighbors_colored.append(
             (len(colored_neighbors), len(adj_list), index)
         )
-    return max(num_neighbors_colored)[2]
+    return max(num_neighbors_colored)[2] if num_neighbors_colored else None
 
 
 if __name__ == '__main__':
     from ai_graph_color import problem_generator
-    generated_problem = problem_generator.generate_graph(5)
+    generated_problem = problem_generator.generate_graph(100)
     print generated_problem, '\n', backtracking_search(generated_problem, 4)
